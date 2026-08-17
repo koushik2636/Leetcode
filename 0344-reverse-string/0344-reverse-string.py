9c4c24a91ad5e365e374ce399855e2739f22cfc2
Sai Koushik <saikoushikeruvaka@gmail.com>
2026-08-17 08:49:11 +0530

    Time: 0 ms (100%), Space: 23.4 MB (45.26%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -5,6 +5,16 @@ class Solution:
         """
         # s[:]=s[::-1]
 
-        s.reverse()
+        #s.reverse()
+
+        i=0
+        j=len(s)-1
+        while i<j:
+            temp=s[i]
+            s[i]=s[j]
+            s[j]=temp
+            i+=1
+            j-=1
+        
         
         
