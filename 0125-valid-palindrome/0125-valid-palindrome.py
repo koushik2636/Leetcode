d038391e806d9d664d31751f5d573e503a78a254
Sai Koushik <saikoushikeruvaka@gmail.com>
2026-08-17 08:39:22 +0530

    Time: 5 ms (87.77%), Space: 19.6 MB (76.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,32 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        for i in s:
-            if not (65<=ord(i)<=90 or 97<=ord(i)<=122 or 48<=ord(i)<=57):
-                s=s.replace(i,"")
+        # for i in s:
+        #     if not (65<=ord(i)<=90 or 97<=ord(i)<=122 or 48<=ord(i)<=57):
+        #         s=s.replace(i,"")
                
-        s=s.lower()
-        if s==s[::-1]:
-            return True
-        else:
-            return False
+        # s=s.lower()
+        # if s==s[::-1]:
+        #     return True
+        # else:
+        #     return False
 
                 
+        #two pointer
 
+        s=s.lower()
+        i=0
+        j=len(s)-1
+
+        while i<j:
+
+            if not s[i].isalnum():
+                i+=1
+            elif not s[j].isalnum():
+                j-=1
+            elif s[i]==s[j]:
+                i+=1
+                j-=1
+            else:
+                return False
+        return True
         
