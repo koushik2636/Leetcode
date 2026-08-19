f258b67069556563ec2d36244f94e44c7d01ec34
Sai Koushik <saikoushikeruvaka@gmail.com>
2026-08-19 12:34:21 +0530

    Time: 11 ms (77.34%), Space: 19.4 MB (46.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,28 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # freq1={}
-        # for i in s:
-        #     if i not in freq1:
-        #         freq1[i]=1
-        #     else:
-        #         freq1[i]+=1
-        # freq2={}
-        # for j in t:
-        #     if j not in freq2:
-        #         freq2[j]=1
-        #     else:
-        #         freq2[j]+=1
-        # if freq1==freq2:
-        #     return True
-        # return False
-
-        l1=list(s)
-        l2=list(t)
-        l1.sort()
-        l2.sort()
-        if str(l1)==str(l2):
-            return True
-        return False
-
-        
+        if len(s)!=len(t):
+            return False
+        freq={}
+        for i in s:
+            if i not in freq:
+                freq[i]=1
+            else:
+                freq[i]+=1
+        for i in t:
+            if i not in freq:
+                return False
+            else:
+                freq[i]-=1
+        for i in freq.values():
+            if i!=0:
+                return False
+        return True
         
