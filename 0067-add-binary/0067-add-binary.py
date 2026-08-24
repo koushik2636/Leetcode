b6843e0d353c9f9a26aa5852983727616c449c34
Sai Koushik <saikoushikeruvaka@gmail.com>
2026-08-24 20:40:46 +0530

    Time: 1 ms (54.65%), Space: 19.4 MB (17.39%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,28 +1,32 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        x=int(a)
-        y=int(b)
-        res1=0
-        res2=0
-        i=0
-        j=0
-        while x>0:
+        # x=int(a)
+        # y=int(b)
+        # res1=0
+        # res2=0
+        # i=0
+        # j=0
+        # while x>0:
     
-            d=x%10
-            res1+=d*(2**i)
-            x//=10
-            i+=1
+        #     d=x%10
+        #     res1+=d*(2**i)
+        #     x//=10
+        #     i+=1
 
-        while y>0:
+        # while y>0:
     
-            d=y%10
-            res2+=d*(2**j)
-            y//=10
-            j+=1
+        #     d=y%10
+        #     res2+=d*(2**j)
+        #     y//=10
+        #     j+=1
 
-        bi=bin(res1+res2)
-        str1=str(bi)
-        return str1[2:]
+        # bi=bin(res1+res2)
+        # str1=str(bi)
+        # return str1[2:]
+
+        x=int(a,2)
+        y=int(b,2)
+        return bin(x+y)[2::]
         
 
 
