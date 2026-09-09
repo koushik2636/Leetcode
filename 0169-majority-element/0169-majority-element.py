e62c179a8c44e0b551db4c270946ff4b96d21d0c
Sai Koushik <saikoushikeruvaka@gmail.com>
2026-09-09 18:46:07 +0530

    Time: 7 ms (57.88%), Space: 21.2 MB (18.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,29 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n=len(nums)
+        # n=len(nums)
         # nums.sort()
         # return nums[n//2]
 
-        dict1={}
-        for i in range(n):
-            if nums[i] not in dict1:
-                dict1[nums[i]]=1
+        # dict1={}
+        # for i in range(n):
+        #     if nums[i] not in dict1:
+        #         dict1[nums[i]]=1
+        #     else:
+        #         dict1[nums[i]]+=1
+        # for k,v in dict1.items():
+        #     if v>n//2:
+        #         return k
+
+        candidate = None
+        vote=0
+        for i in range(len(nums)):
+            if vote==0:
+                candidate=nums[i]
+                vote=1
+            elif nums[i]==candidate:
+                vote+=1
             else:
-                dict1[nums[i]]+=1
-        for k,v in dict1.items():
-            if v>n//2:
-                return k
+                vote-=1
+        return candidate
+
         
