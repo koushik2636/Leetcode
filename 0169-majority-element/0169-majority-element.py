537e621e4c7e33fefb74786a7d0687690e7570ae
Sai Koushik <saikoushikeruvaka@gmail.com>
2026-09-09 18:15:21 +0530

    Time: 2 ms (87.07%), Space: 21.3 MB (18.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        checked=[]
-        
-        for i in range(len(nums)):
-            if nums[i] not in checked:
-                checked.append(nums[i])
-                if nums.count(nums[i])> len(nums)/2:
-                    return nums[i]
+        n=len(nums)
+        nums.sort()
+        return nums[n//2]
         
